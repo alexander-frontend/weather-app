76ef8e2e37cec6eb1c93f97b9c0d7a8eab4a6969
Alex Loy <taekwondist4@gmail.com>
2023-10-26 00:44:01 +0300

    fix:

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import HomeView from '../views/HomeView.vue';
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import FavoritesView from '@/views/FavoritesView.vue';
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     {
       path: '/weather-app',
